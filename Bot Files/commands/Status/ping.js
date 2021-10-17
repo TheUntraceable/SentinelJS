5cfe72779ceaec6ecbb467347a7592f0cc66932f
Armxxn34 <73848907+Armxxn34@users.noreply.github.com>
2021-10-17 12:22:28 +0100

    Update ping.js

--- Bot Files/commands/Status/ping.js
+++ Bot Files/commands/Status/ping.js
@@ -1,13 +1,18 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('ping')
-		.setDescription('Replies with the Clients latency!'),
-	cooldowns : new Set(),
-	cooldown : 10,
-	category : "status",
+		.setDescription('Ping, pong, am I online?'),
 	async execute(interaction) {
-		await interaction.reply({content : `Client Latency - ${interaction.client.ws.ping}ms`});
-	}
+    const ping = interaction.client.ws.ping;
+
+        const pingy = new MessageEmbed()
+	.setColor('GREEN')
+	.setTitle("Wait a second.. Ping?")
+	.setDescription(`WOAH My response time is \`${ping} ms\``)
+	.setTimestamp()
+		return interaction.reply({ embeds: [pingy] });
+	},
 };
