b390cb8a9825b08452d49e3f96eaed8fa0269252
Armxxn34 <73848907+Armxxn34@users.noreply.github.com>
2021-10-17 12:25:44 +0100

    Create invite.js

---
+++ Bot Files/commands/Status/invite.js
@@ -0,0 +1,18 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('invite')
+		.setDescription('invite the bot'),
+	async execute(interaction) {
+ const botAdd = `[Thank you for adding me!](invite link here
+)`
+        const pingy = new MessageEmbed()
+	.setColor('RANDOM')
+	.setTitle("Invite Link!")
+	.setDescription(botAdd)
+	.setTimestamp()
+		return interaction.reply({ embeds: [pingy] });
+	},
+};
