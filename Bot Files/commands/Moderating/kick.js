2276535d6393ae01d03dfc16f417b9d43f18cd73
TheUntraceable <ridhwanplayzzz@gmail.com>
2021-10-19 07:57:02 +0100

    Work on school

--- Bot Files/commands/Moderating/kick.js
+++ Bot Files/commands/Moderating/kick.js
@@ -4,8 +4,13 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('kick-with-id')
-        .setDescription('Kicks a member using their ID.'),
-    usage : "/kick <user-id> [reason]",
+        .setDescription('Kicks a member using their ID.')
+        .addStringOption(option =>
+            option.setName("member-id")
+            .setDescription("The ID of the member you would like to kick.")
+            .required(true)
+            ),
+        usage : "/kick <user-id> [reason]",
 	cooldowns : new Set(),
 	cooldown : 10,
 	category : "moderating",
@@ -15,15 +20,17 @@ module.exports = {
         
         if(!interaction.member.permissions.has("KICK_MEMBERS")) { 
             return await interaction.reply({ephemeral : true,embeds : [new MessageEmbed.setColor("#ff0000").setTitle="You do not have the `KICK_MEMBERS` permission.".setDescription("You need `KICK_MEMBERS` to execute this command. Try again once you are sure you have this permission.")]})
-        }
-
+        }  
+        
         user = interaction.guild.members.cache.get(interaction.options.get("id"))
 
-        const embed = new MessageEmbed().setTitle(`You have been kicked from: ${interaction.guild.name} by ${interaction.member.name}.\n${reason ?? "No reason was specified."}`).setColor("#ff0000")
-        try (
+        if(!user) await interaction.reply({content : "That ID is invalid."})
+        const embed = new MessageEmbed().setTitle(`You have been kicked from: ${interaction.guild.name} by ${interaction.member.name}.\n${reason ? `Reason: ${reason}` : "No reason was specified."}`).setColor("#ff0000")
+        try {
             user.send({embeds : [embed]})
-        ) catch error {
-
+        } catch (error) {
+            console.error(error)
         }
+        await
         }
     }
