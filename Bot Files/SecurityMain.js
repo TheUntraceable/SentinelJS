39c5d522855d0f3b9a89f345f9728dc4890ec2f0
TheUntraceable <ridhwanplayzzz@gmail.com>
2021-10-26 21:00:49 +0100

    Update core bot file

--- Bot Files/SecurityMain.js
+++ Bot Files/SecurityMain.js
@@ -1,6 +1,6 @@
 const config = require('./config.json');
 const { Client, Collection, Intents } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS,Intents.FLAGS.GUILD_MEMBERS] }); 
+const client = new Client({ intents: [Intents.FLAGS.GUILDS,Intents.FLAGS.GUILD_MEMBERS,Intents.FLAGS.GUILD_MESSAGES] }); 
 const fs = require('fs');
 
 client.commands = new Collection();
