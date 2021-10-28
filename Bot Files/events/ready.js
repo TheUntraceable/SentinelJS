8e20f410f7ef5851276142e1441b82f4b77dce64
TheUntraceable <ridhwanplayzzz@gmail.com>
2021-10-28 20:10:23 +0100

    Add configuration for every guild.

--- Bot Files/events/ready.js
+++ Bot Files/events/ready.js
@@ -4,11 +4,13 @@ module.exports = {
 
 	once: true,
 
-	execute(client) {
+	async execute(client) {
 		console.log(`Ready! Logged in as ${client.user.tag}`);
 		for(command of client.command_names) {
 			client.data_analysis[command] = 0
 		}
+
+		client.guilds.cache.each(guild => await client.openAccount(guild))	
 	}
 
 }
