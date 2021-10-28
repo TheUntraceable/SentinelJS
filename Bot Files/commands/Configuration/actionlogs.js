25d6d285acee6fe036a20e8b923f453d830298aa
TheUntraceable <ridhwanplayzzz@gmail.com>
2021-10-28 20:13:31 +0100

    Go back on idea to add new features on launch

--- Bot Files/commands/Configuration/actionlogs.js
+++ Bot Files/commands/Configuration/actionlogs.js
@@ -5,6 +5,7 @@ module.exports = {
     data : new SlashCommandBuilder()
     .setName("action-logs")
     .setDescription("Set the channel where I should send logs regarding actions.")
+    
     .addChannelOption(option =>
         option
         .setName("channel")
