936425336e7c5b90050b47727d4e97523630bde8
TheUntraceable <ridhwanplayzzz@gmail.com>
2021-10-28 20:02:59 +0100

    Add a nice lookin embed when commands are changed (untested)

--- Bot Files/events/applicationCommandCreate.js
+++ Bot Files/events/applicationCommandCreate.js
@@ -1,1 +1,20 @@
-work pleasee
+const { MessageEmbed } = require("discord.js")
+module.exports = {
+
+	name: 'applicationCommandCreate',
+
+	once: false,
+
+	async execute(command) {
+        console.log(`New command: ${command.name}.`)
+
+        command.client.guilds.fetch("901801807498608710").then(guild =>
+
+            guild.channels.fetch("901801807498608710").then(channel =>
+
+                channel.send({embeds : [new MessageEmbed().setColor("#51ff00").setTitle("New Command!").setDescription(`<:moderationlow:902500574677843968> There is a new command: ${command.name}. Check it out!`)]})
+
+                )
+            )
+    }
+}
--- Bot Files/events/applicationCommandUpdate.js
+++ Bot Files/events/applicationCommandUpdate.js
@@ -1,1 +1,21 @@
-work pleasee
+const { MessageEmbed } = require("discord.js")
+
+module.exports = {
+
+	name: 'applicationCommandUpdate',
+
+	once: false,
+
+	async execute(command) {
+        console.log(`Deleted command: ${command.name}.`)
+
+        command.client.guilds.fetch("901801807498608710").then(guild =>
+
+            guild.channels.fetch("901801807498608710").then(channel =>
+
+                channel.send({embeds : [new MessageEmbed().setColor("YELLOW").setTitle("Command deleted!").setDescription(`<:moderationmedium:902500576007426068> ${command.name} has been updated.`)]})
+
+                )
+            )
+    }
+}
