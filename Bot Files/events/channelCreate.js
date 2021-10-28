a1484fdf1a097d8d9d15a1b8de9df02a31d9b1b4
TheUntraceable <ridhwanplayzzz@gmail.com>
2021-10-28 20:11:28 +0100

    Add events ()

--- Bot Files/events/channelCreate.js
+++ Bot Files/events/channelCreate.js
@@ -1,1 +1,18 @@
-work pleasee
+const { MessageEmbed } = require("discord.js")
+
+module.exports = {
+
+	name: 'channelCreate',
+
+	once: false,
+
+	async execute(channel) {
+        const id = await channel.client.mongo.guilds.find({})
+
+        channel.guild.channels.fetch(id).then(channel =>
+        
+            channel.send({embeds : [new MessageEmbed().setColor("YELLOW").setTitle("Command deleted!").setDescription(`<:moderationmedium:902500576007426068> ${command.name} has been updated.`)]})
+
+        )
+    }
+}
