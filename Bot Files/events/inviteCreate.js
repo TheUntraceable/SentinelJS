2dc0d60ca51e80e99ac5924489ef147d00a1f9e2
TheUntraceable <ridhwanplayzzz@gmail.com>
2021-10-29 21:40:28 +0100

    Add event logging regarding invites

--- Bot Files/events/inviteCreate.js
+++ Bot Files/events/inviteCreate.js
@@ -1,1 +1,20 @@
-work pleasee
+const { MessageEmbed } = require("discord.js")
+
+module.exports = {
+    name: "inviteCreate",
+    once: false,
+    async execute(invite) {
+        const data = await invite.client.db.guilds.findOne({guildId : invite.guild.id})
+        if (data.actionLogs != false) {
+            const embed = new MessageEmbed()
+            .setTitle("A new invite has been created!")
+            .setDescription(`An invite with the code ${invite.code} has been created!`)
+            .setColor("#51ff00")
+            .setTimestamp()
+
+            invite.client.channels.fetch(data.actionLogs).then(channel =>
+                channel.send({embeds : [embed]})
+                )
+        }
+    }
+}
--- Bot Files/events/inviteDelete.js
+++ Bot Files/events/inviteDelete.js
@@ -1,1 +1,20 @@
-work pleasee
+const { MessageEmbed } = require("discord.js")
+
+module.exports = {
+    name: "inviteDelete",
+    once: false,
+    async execute(invite) {
+        const data = await invite.client.db.guilds.findOne({guildId : invite.guild.id})
+        if (data.actionLogs != false) {
+            const embed = new MessageEmbed()
+            .setTitle("An invite has been deleted!")
+            .setDescription(`An invite with the code ${invite.code} has been deleted!`)
+            .setColor("#ff0000")
+            .setTimestamp()
+
+            invite.client.channels.fetch(data.actionLogs).then(channel =>
+                channel.send({embeds : [embed]})
+                )
+        }
+    }
+}
