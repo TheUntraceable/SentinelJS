9529b5a18a424aa53bcc243bf62a36367d0d4384
TheUntraceable <ridhwanplayzzz@gmail.com>
2021-10-30 12:52:25 +0100

    Add logging regarding stickers

--- Bot Files/events/stickerCreate.js
+++ Bot Files/events/stickerCreate.js
@@ -1,1 +1,19 @@
-work pleasee
+const { MessageEmbed } = require("discord.js")
+
+module.exports = {
+    name: "stickerCreate",
+    once: false,
+    async execute(sticker) { // I keep on typing `sticket` for some reason...
+        const data = await sticker.client.db.guilds.findOne({guildId: sticker.guild.id}) 
+        if(data.actionLogs != false) {
+            const embed = new MessageEmbed()
+            .setTitle("A new sticker has been created!")
+            .setDescription(`${sticker.name} has been created!`)
+            .setColor("#51ff00")
+            .setTimestamp()
+            sticker.client.channels.fetch(data.actionLogs).then(channel =>
+                channel.send({embeds: [embed]})
+                )
+        }
+    }
+}
--- Bot Files/events/stickerUpdate.js
+++ Bot Files/events/stickerUpdate.js
@@ -1,1 +1,21 @@
-work pleasee
+const { MessageEmbed } = require("discord.js")
+
+module.exports = {
+    name: "stickerCreate",
+    once: false,
+    async execute(before,after) { // I keep on typing `sticket` for some reason...
+
+        const data = await before.client.db.guilds.findOne({guildId: sticker.guild.id}) 
+
+        if(data.actionLogs != false) {
+            const embed = new MessageEmbed()
+            .setTitle("A sticker has been updated!")
+            .setDescription(`${before.name == after.name ? before.name : `${before.name}/${after.name}`} has been updated!`)
+            .setColor("YELLOW")
+            .setTimestamp()
+            after.client.channels.fetch(data.actionLogs).then(channel =>
+                channel.send({embeds: [embed]})
+                )
+        }
+    }
+}
