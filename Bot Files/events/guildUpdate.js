9deaf90b05d46f9434387a40349130bf3a5b8a8b
TheUntraceable <ridhwanplayzzz@gmail.com>
2021-10-30 12:16:32 +0100

    Add GuildUpdate to finish all events related to Guilds

--- Bot Files/events/guildUpdate.js
+++ Bot Files/events/guildUpdate.js
@@ -1,1 +1,22 @@
-work pleasee
+const { MessageEmbed } = require("discord.js")
+
+module.exports = {
+    name: "guildUpdate",
+    once: false, // I really have the need to make a joke here but I can't think of one.
+    async execute(before,after) {
+
+        const data = await before.client.db.guilds.findOne({guildId: before.id})
+
+        if(data.actionLogs != false) {
+            const embed = new MessageEmbed()
+            .setName("Guild Update!")
+            .setDescription(`${before.name == after.name ? before.name : `${before.name}/${after.name}`} has been updated!`)
+            .setColor("YELLOW")
+            .setTimestamp()
+
+            after.client.channels.fetch(data.actionLogs).then(channel =>
+                channel.send({embeds: [embed]})
+                )
+        }
+    }
+}
