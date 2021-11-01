0c639845b8e3dd311dafed61072f714e2614a90e
TheUntraceable <ridhwanplayzzz@gmail.com>
2021-11-01 11:26:25 +0000

    Add intents

--- Bot Files/SecurityMain.js
+++ Bot Files/SecurityMain.js
@@ -2,12 +2,12 @@ const config = require('./config.json');
 const { Client, Collection, Intents } = require('discord.js');
 const fs = require('fs');
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS,Intents.FLAGS.GUILD_MEMBERS,Intents.FLAGS.GUILD_MESSAGES] }); 
+const client = new Client({ intents: [Intents.FLAGS.GUILDS,	Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_BANS, Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS, Intents.FLAGS.GUILD_INTEGRATIONS, Intents.FLAGS.GUILD_WEBHOOKS, Intents.FLAGS.GUILD_INVITES, Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILD_PRESENCES, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.GUILD_MESSAGE_TYPING]});
+// I don't need people in my DM's but I do need literally everything else.
 
 client.commands = new Collection();
 client.data_analysis = []
 client.config = config
-client.command_names = new Array();
 
 fs.readdir(`${process.cwd()}/helpers/`, (err, files) => {
 	if (err) { throw err }
