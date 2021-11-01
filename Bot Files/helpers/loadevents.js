8670b539be9ee9fd70ab35a2e2a8a9836edaf02c
TheUntraceable <ridhwanplayzzz@gmail.com>
2021-11-01 11:21:04 +0000

    Add event handler again

--- Bot Files/helpers/loadevents.js
+++ Bot Files/helpers/loadevents.js
@@ -2,21 +2,20 @@ const fs = require("fs")
 module.exports = client => {
     client.loadEvents = () => {
         {
-            /*
+            
             const eventFiles = fs.readdirSync(`${process.cwd()}/events`).filter(file => file.endsWith('.js'));
+
             for (const file of eventFiles) {
+
                 const event = require(`${process.cwd()}/events/${file}`);
+
                 if (event.once) {
-                    client.once(event.name, (...args) => await event.execute(...args));
+                    client.once(event.name, (...args) => event.execute(...args));
+
                 } else {
-                    client.on(event.name, (...args) => await event.execute(...args));
+                    client.on(event.name, (...args) => event.execute(...args));
                 }
             }
-            */
-           client.on("interactionCreate", interaction => {
-               interaction.client.handle(interaction)
-           })
-            console.log("I didn't load events :D")        
         }
     }
 }
