bb9d39981964586fcd06dd7847cbff57fe93cbaf
TheUntraceable <ridhwanplayzzz@gmail.com>
2021-11-02 19:51:31 +0000

    Fix TypingStart event

--- Bot Files/events/typingStart.js
+++ Bot Files/events/typingStart.js
@@ -1,1 +1,13 @@
-work pleasee
+const { time } = require("@discordjs/builders")
+
+module.exports = {
+    name: "typingStart",
+    once: false,
+    async execute(typing) {
+        const data = await typing.client.db.afk.findOne({where: typing.guild.id,owner: typing.user.id})
+
+        if(!data) return;
+        
+        typing.channel.send(`${typing.user.tag} is back from being AFK! They were AFK for ${time(data.when),"R"}`)
+    }
+}
