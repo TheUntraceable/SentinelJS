be14524e73cb885791e98d7bf50c9110f100bdf8
TheUntraceable <ridhwanplayzzz@gmail.com>
2021-11-02 19:43:38 +0000

    Fix memberUpdate event

--- Bot Files/events/guildMemberUpdate.js
+++ Bot Files/events/guildMemberUpdate.js
@@ -1,1 +1,20 @@
-work pleasee
+const { MessageEmbed } = require("discord.js")
+
+module.exports = {
+    name: "guildMemberUpdate",
+    once: false,
+    async execute(member) {
+        const data = await member.client.guilds.findOne({ guildId: member.guild.id})
+        if(!data.memberUpdates == null) {
+            const embed = new MessageEmbed()
+            .setTitle("A member has been updated!")
+            .setDescription(`${member.tag} has been updated!`)
+            .setColor("YELLOW")
+            .setTimestamp()
+            
+            member.client.channels.fetch(data.memberUpdates).then(channel =>
+                channel.send({embeds: embed})
+                )
+        } 
+    }
+}
