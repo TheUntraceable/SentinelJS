da226f20174fd1509df046e1b7d72b2c9afcfd63
TheUntraceable <ridhwanplayzzz@gmail.com>
2021-11-02 20:27:39 +0000

    Add balance command

--- Bot Files/commands/Fun/balance.js
+++ Bot Files/commands/Fun/balance.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder } = require("@discordjs/builders")
+const { MessageEmbed } = require("discord.js")
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -10,5 +11,30 @@ module.exports = {
         .setDescription("The member's balance you would like to see.")
         .setRequired(false)
         ),
-    implemented: false
+    async execute(interaction) {
+        await interaction.client.openBank(interaction.member)
+        const member = interaction.options.getMember("member") == null ? interaction.member : interaction.options.getMember("member")
+        let data = await interaction.client.db.users.findOne({memberId: member.id})
+
+        // Let's format this data
+
+        const embed = new MessageEmbed()
+            .setTitle(`${member.displayName}'s balance.`)
+            .addFields([
+                {
+                    name: "Wallet",
+                    value: `**\`${data.wallet.toString()}\`**`,
+                    inline: true
+                }, {
+                    name: "Bank",
+                    value: `**\`${data.bank.toString()}\`**/**\`${data.maxBank.toString()}\`**`,
+                    inline: true
+                }, {
+                    name: "Passive?",
+                    value: `**\`${data.passive.toString()}\`**`,
+                    inline: true
+                } 
+            ])
+        await interaction.reply({embeds: [embed]})
+    }
 }
