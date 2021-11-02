e93a23856da19fee7ce20f0cb75d232e52f5ae95
TheUntraceable <ridhwanplayzzz@gmail.com>
2021-11-02 19:36:49 +0000

    Fix GuildMembersChunk Event

--- Bot Files/events/guildMembersChunk.js
+++ Bot Files/events/guildMembersChunk.js
@@ -1,5 +1,7 @@
 module.exports = {
     name: "guildMembersChunk",
     once: false, // Fortunately I ran out of terrible jokes.
-    async execute()
+    async execute(members,guild,chunkData) {
+        console.log(`I have chunked ${members.size} from ${guild.name}. Proud of me?`)
+    }
 }
