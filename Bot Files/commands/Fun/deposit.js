2d80254eceea71a85335aa77aaf850a12dea03e0
TheUntraceable <ridhwanplayzzz@gmail.com>
2021-11-03 21:30:54 +0000

    Make DEposit Command work

--- Bot Files/commands/Fun/deposit.js
+++ Bot Files/commands/Fun/deposit.js
@@ -10,5 +10,20 @@ module.exports = {
         .setDescription("The amount of coins you would like to deposit.")
         .setRequired(true)
         ),
-    implemented: false
+    async execute(interaction) {
+        const data = await interaction.client.db.users.findOne({memberId: interaction.user.id})
+        const amount = interaction.options.getInteger("amount")
+        
+        if(data.wallet < amount) {
+            return await interaction.reply(`You do not have that much money in your wallet! You only have ${data.wallet}${interaction.client.config.cash_emoji}`)
+        
+        } else if(data.maxBank < amount + data.bank) {
+            return await interaction.reply(`You can't fit that much money in your bank! You only have space for ${data.maxBank}${interaction.client.config.cash_emoji}, not ${amount + data.bank}${interaction.client.config.cash_emoji}`)
+        }
+
+        await interaction.client.db.users.updateOne({memberId: interaction.user.id},{$inc : {wallet: -amount, bank: amount}})
+        await interaction.reply(`I have deposited ${amount} for you to your bank account. You now have ${data.bank + amount}/${data.maxBank}${interaction.client.config.cash_emoji} to your bank.`)
+
+
+    }
 }
