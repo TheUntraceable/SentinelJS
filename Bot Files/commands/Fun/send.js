782637462d6a6055fb94d4db83e79e668ddc7d69
TheUntraceable <ridhwanplayzzz@gmail.com>
2021-11-03 21:41:00 +0000

    Make send command work

--- Bot Files/commands/Fun/send.js
+++ Bot Files/commands/Fun/send.js
@@ -9,5 +9,27 @@ module.exports = {
         .setName("member")
         .setDescription("The user you would like to send coins to.")
         .setRequired(true)
-        )   
+        )
+    .addIntegerOption(option =>
+        option
+        .setName("amount")
+        .setDescription("The amount you would like to send.")
+        .setRequired(true)
+        ),
+    async execute(interaction) {
+        const receiver = interaction.options.getMember("member")
+        const amount = interaction.options.getInteger("amount")
+        await interaction.client.openBank(receiver)
+
+        const dataOnInvoker = await interaction.client.db.users.findOne({memberId: interaction.member.id})
+        
+        if(amount > dataOnInvoker.wallet) {
+            return await interaction.reply(`You do not have that much money in your wallet! You only have ${dataOnInvoker.wallet}${interaction.client.config.cash_emoji}.`) // In future make it so that the money is taken out of bank if possible
+        }
+        await interaction.client.db.users.updateOne({memberId: receiver.id}.{$inc : {wallet: amount}})
+        await interaction.client.db.users.updateOne({memberId: interaction.member.id},{$inc : {wallet: -amount}})
+
+        return await interaction.reply(`I have given ${receiver.tag} ${amount}${interaction.client.config.cash_emoji} for you.`)
+
+    }
 }
