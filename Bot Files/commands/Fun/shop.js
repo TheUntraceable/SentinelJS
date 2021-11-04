f34cfde9ba6ae07f53b2d6287131747c80208301
TheUntraceable <ridhwanplayzzz@gmail.com>
2021-11-04 17:51:04 +0000

    Make shop command

--- Bot Files/commands/Fun/shop.js
+++ Bot Files/commands/Fun/shop.js
@@ -13,5 +13,20 @@ module.exports = {
         .addChoice("water gun","water gun")
         .addChoice("durex","durex")
         ),
-    implemented: false
+    async execute(interaction) {
+        const item = interaction.getStringOption("item")
+        if (item == "kids") {
+            interaction.reply(`You can buy kids for 500${interaction.client.config.cash_emoji}.`)
+
+        } else if (item == "water gun") {
+            interaction.reply(`You can buy some durex for 150${interaction.client.config.cash_emoji}.`)
+        
+        } else if (item == "durex") {
+            interaction.reply(`You can buy a water gun for 250${interaction.client.config.cash_emoji}.`)
+        
+        } else {
+            interaction.reply(`You can buy a water gun for 250${interaction.client.config.cash_emoji}.\nYou can buy some durex for 150${interaction.client.config.cash_emoji}.\nYou can buy kids for 500${interaction.client.config.cash}.`)
+
+        }
+    }
 }
