d95ad7e6ee433d226734ae625dc48a7aea98f5dd
TheUntraceable <ridhwanplayzzz@gmail.com>
2021-11-05 08:13:38 +0000

    Rework Data Analysis

--- Bot Files/events/ready.js
+++ Bot Files/events/ready.js
@@ -6,11 +6,6 @@ module.exports = {
 
 	async execute(client) {
 		console.log(`Ready! Logged in as ${client.user.tag}`);
-		client.commands.forEach((v,k,coll) =>  {
-			client.data_analysis[k] = 0 // K = name
-			}
-		)
-
 		client.guilds.cache.each(guild => client.openAccount(guild))	
 	}
 
