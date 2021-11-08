875968bb49e2753c5c15588e5546ff571c1a0920
TheUntraceable <ridhwanplayzzz@gmail.com>
2021-11-08 21:27:01 +0000

    Add buy and sell command UNSTABLE

--- Bot Files/commands/Fun/buy.js
+++ Bot Files/commands/Fun/buy.js
@@ -10,16 +10,18 @@ async function buyItems(interaction) {
     const data = await interaction.client.db.users.findOne({memberId: interaction.member.id})
     
     for(item of mainshop) {
-        if (item.name == wantedItem) {
+        if (item.name == wantedItem) { // wantedItem is just a string
+            
             if (data.money < item.price * amount) {
                 return await interaction.reply(`That item costs ${item.price}${interaction.client.config.cash_emoji} and you only have ${data.wallet}${interaction.client.config.cash_emoji}.`)
             }
-            if(data.inventory.includes(item)) {
+
+            if(data.inventory.find(i => i.name == item.name) != undefined) { // If the item is in their inventory
                 data.inventory[data.inventory.indexOf(item)].amount += amount
                 await interaction.client.db.users.updateOne({memberId: interaction.member.id}, {$set: {wallet: data.wallet - item.price, inventory: data.inventory}})
-                return await interaction.reply(`You bought ${wantedItem} x${amount}.`)
+                return await interaction.reply(`You bought ${wantedItem} x${amount}. You now have ${data.inventory[data.inventory.indexOf(item)].amount}`)
             } else {
-                data.inventory.push({})
+                data.inventory.push({name: item.name, amount: amount})
                 await interaction.client.db.users.updateOne({memberId: interaction.member.id}, {$set: {wallet: data.wallet - item.price, inventory: data.inventory}})
                 return await interaction.reply(`You bought ${wantedItem} x${amount}.`)
 
@@ -31,22 +33,22 @@ module.exports = {
     data: new SlashCommandBuilder()
     .setName("buy")
     .setDescription("Lets you buy an item from the shop")
+    .addStringOption(option => 
+        option
+        .setName("item")
+        .setDescription("The item you would like to buy.")
+        .setRequired(true)
+        .addChoice("kids","kids")
+        .addChoice("water gun","water gun")
+        .addChoice("durex","durex")
+        )
     .addIntegerOption(option =>
         option
         .setName("amount")
         .setDescription("The amount of items you want to buy")
         .setRequired(true)
-        )
-    .addStringOption(option => 
-        option
-        .setName("item")
-        .setDescription("The item you would like to buy.")
-        .setRequired(false)
-        .addChoice("kids","kids")
-        .addChoice("water gun","water gun")
-        .addChoice("durex","durex")
         ),
     async execute(interaction) {
-
+        buyItems(interaction)
     }
 }
--- Bot Files/commands/Fun/sell.js
+++ Bot Files/commands/Fun/sell.js
@@ -1,5 +1,36 @@
 const { SlashCommandBuilder } = require("@discordjs/builders")
 
+const mainshop = [{"id" : 1,"name":"Durex","price":100,"Description":"Durex | Good Protection."},
+            {"id" : 2,"name":"Kids", "price":10, "Description": "Kids | Good ~~slaves~~ kids."},
+            {"id" : 3,"name" : "Water gun", "price" : 100, "Description": "A water gun! (This update I made a use command)"}
+           ]
+
+async function sellItems(interaction) {
+    const wantedItem = interaction.options.getString("item");
+    const amount = interaction.options.getInteger("amount")
+    const data = await interaction.client.db.users.findOne({memberId: interaction.member.id})
+    
+    for(item of mainshop) {
+        if (item.name == wantedItem) { // wantedItem is just a string
+            
+            if (amount > item.amount) {
+                return await interaction.reply(`You only have ${item.amount} ${item.name} to sell. Not ${amount}.`)
+            }
+
+            if(data.inventory.find(i => i.name == item.name) != undefined) { // If the item is in their inventory
+                data.inventory[data.inventory.indexOf(item)].amount -= amount
+                await interaction.client.db.users.updateOne({memberId: interaction.member.id}, {$set: {wallet: data.wallet + amount * item.price, inventory: data.inventory}})
+                return await interaction.reply(`You sold ${wantedItem} x${amount}. You now have ${data.inventory[data.inventory.indexOf(item)].amount}`)
+            } else {
+                data.inventory.splice(data.inventory.indexOf(item), 1)
+                await interaction.client.db.users.updateOne({memberId: interaction.member.id}, {$set: {wallet: data.wallet + amount * item.price, inventory: data.inventory}})
+                return await interaction.reply(`You sold ${wantedItem} x${amount}.`)
+
+            }
+        }
+    } 
+}
+
 module.exports = {
     data: new SlashCommandBuilder()
     .setName("sell")
@@ -8,10 +39,18 @@ module.exports = {
         option
         .setName("item")
         .setDescription("The item you would like to view.")
-        .setRequired(false)
+        .setRequired(true)
         .addChoice("kids","kids")
         .addChoice("water gun","water gun")
         .addChoice("durex","durex")
+        )
+    .addIntegerOption(option =>
+        option
+        .setName("amount")
+        .setDescription("The amount of this item you'd like to sell.")
+        .setRequired(true)
         ),
-    implemented: false
+    async execute(interaction) {
+        await sellItems(interaction)       
+    }
 }
