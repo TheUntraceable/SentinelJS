7b2f6722177ebe3b095bbc3ba84581ad0e382b75
TheUntraceable <ridhwanplayzzz@gmail.com>
2021-11-10 20:51:47 +0000

    Fix dumb issue

--- Bot Files/events/presenceUpdate.js
+++ Bot Files/events/presenceUpdate.js
@@ -4,16 +4,16 @@ module.exports = {
     name: 'presenceUpdate',
     once: false,
     async execute(before,after) {
-        await before.client.openAccount(before.guild)
-        const data = await before.client.db.guilds.findOne({guildId: before.guild.id})
+        await after.client.openAccount(after.guild)
+        const data = await after.client.db.guilds.findOne({guildId: after.guild.id})
         if(!data.memberUpdates == false) {
             const embed = new MessageEmbed()
             .setTitle("A member's presence has been updated!") // Why do I sound excited?
             .setDescription(`${before.member.user.tag} has updated their presence!`)
             .setColor("YELLOW")
             .setTimestamp()
-            before.client.channels.fetch(memberUpdates).then(channel => channel.send({embeds: [embed]}))
-            
+            after.client.channels.fetch(memberUpdates).then(channel => channel.send({embeds: [embed]}))
+
         }
     }
 }
