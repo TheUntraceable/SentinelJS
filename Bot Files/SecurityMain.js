0914b8d2412ff38e251033c61ea957e274225dca
TheUntraceable <ridhwanplayzzz@gmail.com>
2021-11-11 22:08:07 +0000

    Add statcord

--- Bot Files/SecurityMain.js
+++ Bot Files/SecurityMain.js
@@ -1,12 +1,21 @@
+const Statcord = require("statcord.js");
 const config = require('./config.json');
 const { Client, Collection, Intents } = require('discord.js');
 const fs = require('fs');
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS,	Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_BANS, Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS, Intents.FLAGS.GUILD_INTEGRATIONS, Intents.FLAGS.GUILD_WEBHOOKS, Intents.FLAGS.GUILD_INVITES, Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILD_PRESENCES, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.GUILD_MESSAGE_TYPING]});
+
 // I don't need people in my DM's but I do need literally everything else.
 
 client.commands = new Collection();
 client.config = config
+
+client.statcord = new Statcord.Client({
+    client,
+    key: client.config["statcord-api-key"],
+});
+
+
 
 fs.readdir(`${process.cwd()}/helpers/`, (err, files) => {
 	if (err) { throw err }
@@ -26,5 +35,7 @@ fs.readdir(`${process.cwd()}/helpers/`, (err, files) => {
 	client.loadCommands();
 	client.cacheAntispammers()
 });
+
+
 
 client.login(client.config.token)
--- Bot Files/events/ready.js
+++ Bot Files/events/ready.js
@@ -5,6 +5,7 @@ module.exports = {
 	once: true,
 
 	async execute(client) {
+		client.statcord.autopost();
 		console.log(`Ready! Logged in as ${client.user.tag}`);
 		client.guilds.cache.each(guild => client.openAccount(guild))	
 	}
