a78ac26664e4178708693bdb777ce4008496d9d8
TheUntraceable <ridhwanplayzzz@gmail.com>
2021-11-11 22:08:36 +0000

    Create new AntiSpam instance for new guilds

--- Bot Files/commands/Configuration/antispammer.js
+++ Bot Files/commands/Configuration/antispammer.js
@@ -1,3 +1,4 @@
+const AntiSpam = require('discord-anti-spam');
 const { SlashCommandBuilder } = require("@discordjs/builders")
 const { MessageEmbed } = require("discord.js")
 
@@ -18,7 +19,7 @@ module.exports = {
         if(!interaction.member.permissions.has("MANAGE_GUILD")) {
             return await interaction.reply({ephemeral : true,embeds : [new MessageEmbed().setColor("#ff0000").setTitle("You do not have the `MANAGE_GUILD` permission.").setDescription("You need `MANAGE_GUILD` to execute this command. Try again once you are sure you have this permission.")]})
         }
-        
+        const data = await interaction.client.db.guilds.findOne({guildId: interaction.guild.id})
         await interaction.client.db.guilds.updateOne({
             guildId : interaction.guild.id
         },{
@@ -26,6 +27,30 @@ module.exports = {
                 antispammer: interaction.options.getBoolean("mode") 
             }
         })
+        let antispam = new AntiSpam({
+            warnThreshold : 3,
+            kickThreshold : 7,
+            muteThreshold : 5,
+            banThreshold : 15,
+            maxDuplicatesWarn : 3,
+            maxDuplicatesKick  : 7,
+            maxDuplicatesMute : 5,
+            maxDuplicatesBan : 15,
+            muteRoleName : data.muteRole,
+            modLogsChannelName : data.actionLogs,
+            modLogsEnabled : true,
+            errorMessages : true,
+            warnEnabled : true,
+            kickEnabled : true,
+            muteEnabled : true,
+            banEnabled : true,
+            deleteMessagesAfterBanForPastDays : 14,
+            removeMessages : true,	
+            removeBotMessages : true,	
+            removeBotMessagesAfter : 5000,
+            })
+        interaction.client.antispammers.set(interaction.guild.id, antispam)
+
         return await interaction.reply(`I have ${interaction.options.getBoolean("mode") ? "`enabled`" : "`disabled`"} the antispammer.`)
 
     }
