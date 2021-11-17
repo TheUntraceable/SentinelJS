0379dd6fa3e79cf107bbec39d363b75b78eab47f
TheUntraceable <ridhwanplayzzz@gmail.com>
2021-11-17 20:40:25 +0000

    Add activity changing

--- Bot Files/events/ready.js
+++ Bot Files/events/ready.js
@@ -6,16 +6,13 @@ module.exports = {
 
 	async execute(client) {
 		console.log(`Ready! Logged in as ${client.user.tag}`);
+		client.user.setActivity(`over ${client.users.cache.size} users!`, { type: 'WATCHING' });
 		client.statcord.autopost();
 		client.guilds.cache.forEach(guild => client.openAccount(guild))	
 
 		setInterval(() => {
-
-			client.user.setPresence({
-				name: `over ${client.users.cache.size}`,
-				type: "WATCHING"
-			});
-		}, 5000);
+			client.user.setActivity(`over ${client.users.cache.size} users!`, { type: 'WATCHING' });
+		}, 300000);
 		
 	}
 
