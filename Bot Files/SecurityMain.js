cbb6c4c0060e2baa01699cfb8d9cb0395fe40d90
TheUntraceable <ridhwanplayzzz@gmail.com>
2021-11-20 18:31:57 +0000

    Only deploy slash commands if dev mode == true

--- Bot Files/SecurityMain.js
+++ Bot Files/SecurityMain.js
@@ -3,7 +3,7 @@ const config = require('./config.json');
 const { Client, Collection, Intents } = require('discord.js');
 const fs = require('fs');
 
-const client = new Client({ presence: {status: "dnd"}, intents: [Intents.FLAGS.GUILDS,	Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_BANS, Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS, Intents.FLAGS.GUILD_INTEGRATIONS, Intents.FLAGS.GUILD_WEBHOOKS, Intents.FLAGS.GUILD_INVITES, Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILD_PRESENCES, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.GUILD_MESSAGE_TYPING]});
+const client = new Client({ presence: { status: "dnd" }, intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_BANS, Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS, Intents.FLAGS.GUILD_INTEGRATIONS, Intents.FLAGS.GUILD_WEBHOOKS, Intents.FLAGS.GUILD_INVITES, Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILD_PRESENCES, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.GUILD_MESSAGE_TYPING]});
 
 // I don't need people in my DM's but I do need literally everything else.
 
@@ -33,7 +33,7 @@ fs.readdir(`${process.cwd()}/helpers/`, (err, files) => {
 		await client.cacheAntispammers()
 		client.guilds.cache.forEach(guild => client.openAccount(guild))	
 		client.loadCommands();
-		if(client.config.dev_mode) client.deploy()
+		if(client.config.dev) client.deploy()
 	}).catch(message => {
 		console.error(message)
 		client.destroy()
