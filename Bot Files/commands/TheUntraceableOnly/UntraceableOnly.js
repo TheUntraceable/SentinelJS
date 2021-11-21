ad92dc959452856dd2b05036d8c0ca19dda58cfe
TheUntraceable <ridhwanplayzzz@gmail.com>
2021-11-21 15:22:53 +0000

    Add a command for me to make any command only accessible to me, ish

---
+++ Bot Files/commands/TheUntraceableOnly/UntraceableOnly.js
@@ -0,0 +1,35 @@
+const { SlashCommandBuilder } = require("@discordjs/builders")
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName("UntraceableOnly")
+    .setDescription("Makes a command only available to The Untraceable.")
+    .addIntegerOption(option =>
+        option
+        .setName("command-id")
+        .setDescription("The command's Id you would like to make only available to you.")
+        .setRequired(true)
+        ),
+    async execute(interaction) {
+        const id = interaction.options.getInteger("command-id")
+        
+        if(!interaction.client.application?.owner) await interaction.client.application?.fetch()
+
+        const command = await interaction.client.application.commands.fetch(id)
+        
+        if(!command) {
+            return await interaction.reply("That command doesn't exist, how do you not know your own bot's commands Id when you made a command to show you the Id's for each and every command..")
+        }
+
+        const permissions = [
+            {
+                id: interaction.client.application?.id,
+                type: "USER",
+                permission: true
+            }
+        ]
+
+        await command.permissions.set({ permissions })
+
+    }
+}
