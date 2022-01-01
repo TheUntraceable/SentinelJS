4105c0862ca2648b81cdadd781a9ad81a3d88f45
TheUntraceable <ridhwanplayzzz@gmail.com>
2022-01-01 00:45:41 +0000

    Update generateCommandsPage file

--- Bot Files/generateCommandsPage.js
+++ Bot Files/generateCommandsPage.js
@@ -1,54 +1,75 @@
 const { Collection } = require("discord.js")
 
 class CommandsPageGenerator {
-    constructor(base) {
-        this.base = base; // The base HTML page
+    constructor() {
         this.commands = new Collection();
     }
 
     generateCommandsPage() {
+        let base = "";
+
         for(const command of this.commands.values()) {
+            if(command.category == "TheUntraceableOnly") {
+                continue
+            }
             base += `
-            <div class="command" id=${command.data.name}>
-                <div class="command-name">
-                    <p>Name: ${command.data.name}</p>
-                </div>
-                <div class="command-description">
-                    <p>Description: ${command.data.description}</p>
-                </div>
-                <div class="command-category">
-                    <p>Category: ${command.category}</p>
-                </div>
+<div id = "${command.data.name}" class="command">
+    <div class="command-name">
+        <button onclick="toggle('${command.data.name}')" type="button">${command.data.name}</button>
+    </div>
+    <div id="${command.data.name}-description-category-args-permissions" class="command-description-category-args-permissions">
+        <div id="${command.data.name}-description" class="command-description">
+            <p>Description: ${command.data.description}</p>
+        </div>
+        <div id="${command.data.name}-category"class="command-category">
+            <p>Category: ${command.category}</p>
+        </div>
             `
             if(command.data.options.length > 0) {
                 base += `
-                <div class="command-args">
-                    <p>Arguments: ${command.data.options.map(option => option.name.required ? `<${option.name}>` : `[${option.name}]`).join(" ")}</p>
-                </div>`
+        <div id="${command.data.name}-arg" class="command-args">
+            <p>Arguments: ${command.data.options.map(option => option.required ? `<${option.name}> ${option.description ? `(${option.description})` : ""}` : `[${option.name}] ${option.description ? `(${option.description})` : ""}`).join(", ")}</p>
+        </div>`
             } else {
                 base += `
-                <div class="command-args">
-                    <p>Arguments: None.</p>
-                </div>`                
+        <div id="${command.data.name}-args" class="command-args">
+            <p>Arguments: None.</p>
+        </div>`                
             }
+            if(command.requiredPermissions) {
+                base += `
+        <div id="${command.data.name}-permissions" class="command-permissions">
+            <p>Required Permissions: ${command.requiredPermissions.join(", ")}</p>
+        </div>`
+            } else {
+                base += `
+
+        <div id="${command.data.name}-permissions" class="command-permissions">
+            <p>Required Permissions: None.</p>
+        </div>`
+                }
             base += `
-                <div class="command-usage">
-                    <p>Usage: /${command.usage}</p>
-                </div>
-            </div>
-            `
+    </div>
+</div>`
         }
-        if(!base.endsWith("</html>")) {
-            base += "</html>"
+/* <script>
+    function toggle(commandName) {
+        const mainStuff = document.getElementById(commandName + "-description-category-args-permissions")
+        if(mainStuff.style.visibility != "visible") {
+            mainStuff.style.visibility = "visible"
+        } else {
+            mainStuff.style.visibility = "hidden"
         }
+    }
+</script> */
 
         return base
     } 
 }
 
-base = `<!DOCTYPE HTML>\n<html>\n    <title> Commands Page </title>\n    <h1> Arguments! </h1>\n    <p> If an argument is surrounded by "[]" then the argument is optional, if it is surrounded by "<>", then it is required.</p>\n`
+// base = `<!DOCTYPE HTML>\n<html>\n    <title> Commands Page </title>\n    <h1> Arguments! </h1>\n    <p> If an argument is surrounded by <strong>"[ ]"</strong> then the argument is optional, if it is surrounded by <strong>"<>"</strong>, then it is required.</p>\n`
 
-const generator = new CommandsPageGenerator(base)
+const generator = new CommandsPageGenerator()
 
 require("./helpers/loadcommands.js")(generator)
 generator.loadCommands();
