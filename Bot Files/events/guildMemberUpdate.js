213bbbd3684f90dfdf70eb3da474d7eeca770d7a
TheUntraceable <ridhwanplayzzz@gmail.com>
2022-03-06 12:00:29 +0000

    Fix an issue

--- Bot Files/events/guildMemberUpdate.js
+++ Bot Files/events/guildMemberUpdate.js
@@ -5,7 +5,7 @@ module.exports = {
     once: false,
     async execute(member) {
         await member.client.openAccount(member.guild)
-        const data = await member.client.guilds.db.findOne({ guildId: member.guild.id})
+        const data = await member.client.db.guilds.findOne({ guildId: member.guild.id})
         if(!data.memberUpdates == null) {
             const embed = new MessageEmbed()
             .setTitle("A member has been updated!")
